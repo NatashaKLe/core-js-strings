971cf2dce08b6e7e60cd1448952abe93d461688f
Natashakle <ataska91@gmail.com>
2023-11-19 02:07:51 +0300

    resolved: 15

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -149,8 +149,10 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  if (str.indexOf(value) === -1) return str;
+  str = str.slice(0, str.indexOf(value)) + str.slice(str.indexOf(value) + value.length, str.length);
+  return str;
 }
 
 /**
